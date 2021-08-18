480bde98aa382634cb12c776c5eec3e39ab4b932
yusuf erdogan <a.erdogan2018@gtu.edu.tr>
2021-08-18 17:29:00 +0300

    Initial commit

---
+++ src/test/java/com/yusuferdogan/jokesapp/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusuferdogan.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
